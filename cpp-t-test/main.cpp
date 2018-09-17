d652d39d381246819e79c6fb1f179fa96baca34a
patcurry <currypat1985@gmail.com>
2018-09-17 16:36:45 +0200

    pretty ugly, but a start, got the thing to calculate an average and print it out

--- cpp-t-test/main.cpp
+++ cpp-t-test/main.cpp
@@ -3,10 +3,31 @@
 using namespace std;
 
 // write the math first
+// how do I pass an array into here?
+double mean(double d[], int size)
+{
+    // declare variables
+    int i;
+    double sum = 0;
+    double avg;
+
+    // for each
+    for (i = 0; i < size; i++) {
+        sum += d[i];
+    }
+    avg = double(sum) / size;
+
+    return avg;
+}
 
 // main program
 int main()
 {
-    cout << "T Test!" << endl;
+    double test[5] = {12.1, 14.0, 16.4, 18.3, 19.9};
+    double avg;
+
+    avg = mean( test, 5 );
+
+    cout << avg << endl;
     return 0;
 }
