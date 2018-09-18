9d59b669c5332c39ebfafb77ec20ef069e110900
patcurry <currypat1985@gmail.com>
2018-09-18 11:18:20 +0200

    I am confused, maybe I will switch to a different language for a bit, then I can be more confused

--- cpp-t-test/main.cpp
+++ cpp-t-test/main.cpp
@@ -4,46 +4,27 @@
 
 using namespace std;
 
-// write the math first
-// how do I pass an array into here?
-/*
-        << std::accumulate(std::begin(nums),
-                           std::end(nums),
-                           0.0)
-                           */
 
-
-// how do I get length of the array?
-double mean(double nums[])
+int findSumIter(int nums[], int n)
 {
-      return std::accumulate(std::begin(nums),
-                             std::end(nums),
-                             0.0);
+    if (n <= 0)
+        return 0;
+    return (findSumIter(nums, n - 1) + nums[n - 1]);
 }
 
+int findSum(int d[])
+{
+    int n = sizeof(d) / sizeof(d[0]);
+    return findSumIter(d, n);
+}
 
-// main program
 int main()
 {
-    double test[5] = {12.1, 14.0, 16.4, 18.3, 19.9};
-    double avg;
-
-    avg = mean(test);
-
-    cout << avg << endl;
+    //double test[5] = {12.1, 14.0, 16.45, 18.33, 19.9};
+    //int N = sizeof(test) / sizeof(test[0]); // 
+    int  arr[] = {1, 2, 3, 4, 5, 6};
+    //printf("%f", findSum(arr));
+    printf("%d\n", findSum(arr));
     return 0;
 }
 
-
-
-/*
-int main()
-{
-    double nums[] = {1.1,5.2,3.0,2.8,7.9,8.4,100.49,3.0};
-    std::cout
-        << std::accumulate(std::begin(nums),
-                           std::end(nums),
-                           0.0)
-        << '\n';
-}
-*/
