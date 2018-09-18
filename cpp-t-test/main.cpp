c2391a2ae97de4e09bf22b70e42247c741f93112
patcurry <currypat1985@gmail.com>
2018-09-18 14:19:18 +0200

    copied stuff from webpage, now have to read csv and use those values

--- cpp-t-test/main.cpp
+++ cpp-t-test/main.cpp
@@ -1,30 +1,57 @@
 #include <iostream>
-#include <iterator>
-#include <numeric>
-
+#include <stdio.h>
+#include <math.h>
 using namespace std;
 
-
-int findSumIter(int nums[], int n)
+// lifted the math from geeksforgeeks.com
+// I was trying to get too crazy with functional stuff
+// mean function
+float Mean(float arr[], int n)
 {
-    if (n <= 0)
-        return 0;
-    return (findSumIter(nums, n - 1) + nums[n - 1]);
+    float sum = 0;
+    for (int i = 0; i < n; i++)
+        sum = sum + arr[i];
+    return sum / n;
 }
 
-int findSum(int d[])
+float standardDeviation(float arr[], int n)
 {
-    int n = sizeof(d) / sizeof(d[0]);
-    return findSumIter(d, n);
+    float sum = 0;
+    for (int i = 0; i < n; i++)
+        sum = sum + ((arr[i] - Mean(arr, n)) * (arr[i] - Mean(arr, n)));
+    return sqrt(sum / (n - 1));
+}
+
+float tTest(float arr1[], int n,
+            float arr2[], int m)
+{
+    float mean1 = Mean(arr1, n);
+    float mean2 = Mean(arr2, m);
+    float sd1 = standardDeviation(arr1, n);
+    float sd2 = standardDeviation(arr2, m);
+
+    float t_test = (mean1 - mean2) / 
+                    sqrt(
+                         ((sd1 * sd1) / n) + 
+                         ((sd2 * sd2) / m)
+                    );
+    return t_test;
+
 }
 
 int main()
 {
-    //double test[5] = {12.1, 14.0, 16.45, 18.33, 19.9};
-    //int N = sizeof(test) / sizeof(test[0]); // 
-    int  arr[] = {1, 2, 3, 4, 5, 6};
-    //printf("%f", findSum(arr));
-    printf("%d\n", findSum(arr));
+    float arr1[] = {10, 20, 30, 35, 41};
+    float arr2[] = {1, 3, 4, 5, 10};
+
+    int n = sizeof(arr1) / sizeof(arr1[0]);
+    int m = sizeof(arr2) / sizeof(arr2[0]);
+
+    int dof = n + m - 2;
+
+    cout << "Mean: " << Mean(arr1, n) << endl;
+    cout << "Standard Deviation: " << standardDeviation(arr1, n) << endl;
+    cout << "T Score: " << tTest(arr1, n, arr2, m) << endl;
+    cout << "dof: " << dof << endl;
     return 0;
 }
-
