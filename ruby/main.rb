8064344321e62f03e4e14798f600fc9ef6f4f8f6
patcurry <currypat1985@gmail.com>
2018-09-19 15:11:00 +0200

    ruby seems very flexible, a little too flexible

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,4 +1,4 @@
-array1 = [1.23,2.45,3.67,4.89,5.0]
+array1 = [1,2,3,4,5]
 array2 = [3,4,5,6,7]
 
 # m is the average of of array1
@@ -7,19 +7,6 @@ array2 = [3,4,5,6,7]
 m = array1.inject{ |sum, el| sum + el }.to_f / array1.size
 n = array2.inject{ |sum, el| sum + el }.to_f / array2.size
 """
-
-# another implementation
-# reduce and inject are aliases
-# apparently size and length are also aliases
-def sum(arr)
-    arr.reduce(0.0) { |sum, num| sum + num}
-end
-
-def mean(arr)
-    arr.reduce(0.0) { |sum, num| sum + num } / arr.length
-end
-
-# looks like 'p' is shorthand for 'puts'
 
 class Calculator
     def initialize(arr1, arr2)
@@ -36,10 +23,28 @@ class Calculator
     end
 
     def squareDiffs(arr)
-        arr.map( |a| (a - mean(arr)) * (a - mean(arr))))
+        arr.map { |a| (a - mean(arr)) ** 2 }
     end
 
+    def stdDev(arr)
+        Math.sqrt( sampleStdDevDenom( squareDiffs(arr) ) )
+    end
+
+    def twoSampleTTest(arr1, arr2)
+        numerator = (mean(arr1) - mean(arr2)).abs
+        arr1_denom = stdDev(arr1) / arr1.length
+        arr2_denom = stdDev(arr2) / arr2.length
+        denominator = Math.sqrt(arr1_denom + arr2_denom)
+        dof = arr1.length + arr2.length - 2
+        t_score = numerator / denominator
+        {'T Score': t_score, 'dof': dof}
+    end
+
+    def tTestResult
+        twoSampleTTest(@arr1, @arr2)
+    end
 end
 
-calculator = Calculator.new(1, 2)
-p calculator.sum(array1)
+calculator = Calculator.new(array1, array2)
+#p calculator.sum(array1)
+p calculator.tTestResult 
