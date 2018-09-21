68d6d83eecbfc44f9d91f6d891aa76eb66e59144
patcurry <currypat1985@gmail.com>
2018-09-21 15:55:58 +0200

    again, minor changes in the python file

--- python-t-test/main.py
+++ python-t-test/main.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 
 class TTest:
-
     def __init__(self, arr1, arr2):
         self.arr1 = arr1
         self.arr2 = arr2
@@ -15,33 +14,15 @@ class TTest:
     def mean(self, arr):
         return reduce(self.sum, arr) / len(arr)
 
-    def sampleStdDevDenom(self, arr):
-        """
-        basically the function above (mean) but instead
-        of dividing by the length, we divide by the length
-        minus 1
-        """
+    def sample_std_dev_denom(self, arr):
         return reduce(self.sum, arr) / (len(arr) - 1)
 
-    def squareDiffs(self, arr):
-        """
-        map the function a - b over the array d
-        where a is the value in the array and b is
-        the average of d, then return the square
-        of the difference between the value and
-        the average
-        """
+    def square_diffs(self, arr):
         return list(map(lambda a: (a - self.mean(arr))**2, arr))
 
     def std_dev(self, arr):
-        """
-        subtract each value in the sample from the sample mean
-        then square the result
-        take the sum of the results and divide it by the
-        one less than the sample count 
-        """
-        sd = self.squareDiffs(arr)
-        MSD = self.sampleStdDevDenom(sd)
+        sd = self.square_diffs(arr)
+        MSD = self.sample_std_dev_denom(sd)
         return sqrt(MSD)
 
     def two_sample_t_test(self, arr1, arr2):
@@ -56,8 +37,6 @@ class TTest:
     def ttest_result(self):
         return self.two_sample_t_test(self.arr1, self.arr2)
 
-# get data with one column for male heights and one column for female heights
-# each with ten values
 # pandas makes it nice to the the data
 df = pd.read_csv('./height-data.csv', skipinitialspace=True)
 
