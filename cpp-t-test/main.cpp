e7955d0511df4e4965c5d63b979fba3cb1a630d6
patcurry <currypat1985@gmail.com>
2018-09-24 16:28:21 +0200

    I have everything I want to have working in the cpp file, except for the math

--- cpp-t-test/main.cpp
+++ cpp-t-test/main.cpp
@@ -1,12 +1,81 @@
+
 #include <iostream>
-#include <stdio.h>
+#include <fstream>
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
 using namespace std;
 
 // lifted the math from geeksforgeeks.com
-// I was trying to get too crazy with functional stuff
-// mean function
-double Mean(double arr[], int n)
+
+// prototype functions. why? I donno
+// should these be in a class?
+double mean(double arr[], int n);
+double standard_deviation(double arr[], int n);
+double t_test(double arr1[], int n, double arr2[], int m); // This is not giving me a result that make sense with the two test arrays
+
+int main()
+{
+    // I would rather not do it this way, but
+    // because I know how large the csv file is
+    // I am just going to make two arrays that 
+    // are the correct lengths for the csv file
+    // first get the program working, then refactor
+    double male[10];
+    double female[10];
+
+    string line;
+    ifstream myfile("../height-data.csv");
+
+    if (!myfile.is_open()) {
+        cout << "Unable to open file" << endl;
+    }
+    // skip the first line
+    getline(myfile, line);
+
+    // loop through the rest of the lines
+    while (getline(myfile, line)) {
+        stringstream iss(line);
+
+        // loop through the columns (only 2 of them)
+        // and put the data from the even column into
+        // the male array, and the values from the
+        // odd column into the female array
+        for (int col=0; col < 2; col++) {
+            string val;
+            getline(iss, val, ',');
+
+            if (col % 2 == 0) {
+                male[col] = atof(val.c_str());
+                cout << "m " << male[col] << endl;
+            } else {
+                female[col] = atof(val.c_str());
+                cout << "f " << female[col] << endl;
+            }
+        }
+    }
+
+
+    //double arr1[] = { 10, 20, 30, 40, 50 };
+    //double arr2[] = { 1, 29, 46, 78, 99 };
+
+    //int n = sizeof(arr1) / sizeof(arr1[0]);
+    //int m = sizeof(arr2) / sizeof(arr2[0]);
+    int n = sizeof(male) / sizeof(male[0]);
+    int m = sizeof(female) / sizeof(female[0]);
+
+    int dof = n + m - 2;
+
+    //double result = t_test(arr1, n, arr2, m);
+    double result = t_test(male, n, female, m);
+
+    // return result
+    cout << "T-Score: " << result << ", dof: " << dof << endl;
+    return 0;
+}
+
+double mean(double arr[], int n)
 {
     double sum = 0;
     for (int i = 0; i < n; i++)
@@ -14,52 +83,24 @@ double Mean(double arr[], int n)
     return sum / n;
 }
 
-double standardDeviation(double arr[], int n)
+double standard_deviation(double arr[], int n)
 {
     double sum = 0;
     for (int i = 0; i < n; i++)
-        sum = sum + ((arr[i] - Mean(arr, n)) * (arr[i] - Mean(arr, n)));
+        sum = sum + ((arr[i] - mean(arr, n)) * 
+                     (arr[i] - mean(arr, n)));
     return sqrt(sum / (n - 1));
 }
 
-double tTest(double arr1[], int n,
-            double arr2[], int m)
+double t_test(double arr1[], int n,
+              double arr2[], int m)
 {
-    double mean1 = Mean(arr1, n);
-    double mean2 = Mean(arr2, m);
-    double sd1 = standardDeviation(arr1, n);
-    double sd2 = standardDeviation(arr2, m);
+    double mean1 = mean(arr1, n);
+    double mean2 = mean(arr2, m);
+    double sd1 = standard_deviation(arr1, n);
+    double sd2 = standard_deviation(arr2, m);
 
-    //cout << mean1 << endl;
-    //cout << mean2 << endl;
-    //cout << sd1 << endl;
-    //cout << sd2 << endl;
+    double result = (mean1 - mean2) / sqrt(((sd1 * sd1) / n) + ((sd2 * sd2) / m));
+    return result;
 
-    // something is wrong here, this does not seem
-    // to be returning a double
-    double t_test = (mean1 - mean2) / 
-                    sqrt(
-                         ((sd1 * sd1) / n) + 
-                         ((sd2 * sd2) / m)
-                    );
-    return t_test;
-
-}
-
-int main()
-{
-    double arr1[] = {1.0, 2.0, 3.0, 4.0, 5.0};
-    double arr2[] = {3.0, 4.0, 5.0, 6.0, 7.0};
-
-    int n = sizeof(arr1) / sizeof(arr1[0]);
-    int m = sizeof(arr2) / sizeof(arr2[0]);
-
-    int dof = n + m - 2;
-
-    double result = tTest(arr1, n, arr2, m);
-    
-    cout << "T Score: <THIS IS WRONG> " << tTest(arr1, n, arr2, m) << endl;
-    cout << "dof: " << dof << endl;
-    
-    return 0;
 }
--- cpp-t-test/read-csv.cpp
+++ cpp-t-test/read-csv.cpp
@@ -1,7 +1,83 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
+
+/*
+int main() {
+    string line;
+    ifstream myfile("../height-data.csv");
+
+    if (!myfile.is_open()) {
+        cout << "Unable to open file" << endl;
+    }
+    
+    while (getline (myfile, line)) {
+        cout << line << endl;
+    }
+    
+
+    return 0;
+}
+*/
+
+/*
+void Core::parseCSV()
+{
+    std::ifstream  data("height-data.csv");
+    std::string line;
+    std::vector<std::vector<std::string> > parsedCsv;
+    while(std::getline(data,line))
+    {
+        std::stringstream lineStream(line);
+        std::string cell;
+        std::vector<std::string> parsedRow;
+        while(std::getline(lineStream,cell,','))
+        {
+            parsedRow.push_back(cell);
+        }
+
+        parsedCsv.push_back(parsedRow);
+    }
+};
+*/
+
+
+/*
+int main()
+{
+
+    float data[2][11];
+    ifstream file("../height-data.csv");
+
+    for (int row = 0; row < 2; row++) {
+        string line;
+        getline(file, line);
+        if ( !file.good() )
+            break;
+        
+        stringstream iss(line);
+
+        for (int col = 0; col < 11; col++) {
+            string val;
+            getline(iss, val, ',');
+            if ( !iss.good() )
+                break;
+
+            stringstream converter(val);
+            converter >> data[row][col];
+        }
+    }
+
+    cout << data[1][2] << endl;
+
+    return 0;
+}
+*/
+
+
+/*
 int populate(double array[], int number);
 void spit(double array1[], int number);
 int main()
@@ -51,3 +127,4 @@ void spit(double array1[], int number)
         cout << "The value of item " << (i+1) << " is " << array1[i] << endl;
     }
 }
+*/
